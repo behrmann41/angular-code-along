346f730281a0c15729e3887b16775632211916cd
Ian Behrmann <ianjbehrmann@gmail.com>
2015-11-10 12:05:21 -0700

    reaches out to API for data within Angular

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/api/v1/guitars', function (req, res, next){
+  res.json([
+    {id: 1, make: 'Martin', model: 'D15'},
+    {id: 1, make: 'Fender', model: 'Stratocaster'}
+  ])
+})
+//always make wildcard route last or move to app.js
 router.get('*', function(req, res, next) {
   res.sendFile('index.html', {
     root: __dirname + '/../public/'
